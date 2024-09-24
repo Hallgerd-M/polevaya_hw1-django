90714a8267b16fd7f605c48a46900d4753f22c85
Irina Polevaya <mirwen@yandex.ru>
2024-09-24 14:57:57 +0300

    version 1.1 data oved to src

--- src/get_full_path.py
+++ src/get_full_path.py
@@ -1,7 +1,7 @@
 import os
 
 def root_join(*parts):
-    """ Функция которая возвращает путь к файлу указанному из корня """
+    """ Функция которая возвращает полный путь к файлу """
     base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
     full_path = os.path.join(base_dir, *parts)
     return full_path
